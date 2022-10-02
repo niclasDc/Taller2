ddb1972ec39dc95fae2c0d5eeb68bb92ad3922c2
Nicolas Alvarez <nialvarezt@unal.edu.co>
2022-10-02 17:43:38 -0500

    Add querys

--- ejercicio1_parteb.sql
+++ ejercicio1_parteb.sql
@@ -1,6 +1,6 @@
 /* 1. Consulta para traer el nombre del paciente, nombre del doctor y fecha
  programada que estan relacionados en un cita medica*/
- 
+
 SELECT pa.nombre_paciente , doc.nombre_doctor, cm.estado FROM public.paciente AS pa
 INNER JOIN public.cita_medica AS cm ON pa.cedula_paciente = cm.cedula_paciente
 INNER JOIN public.doctor AS doc ON cm.cedula_doctor = doc.cedula_doctor;
@@ -25,35 +25,35 @@ SELECT pa.nombre_paciente, count(cm.codigo_citamedica)
 FROM public.paciente AS pa INNER JOIN public.cita_medica AS cm
 ON pa.cedula_paciente = cm.cedula_paciente GROUP BY pa.nombre_paciente;
 
-/* 5. Cuenta el numero de citas que tiene cada doctor*/
+/* 6. Cuenta el numero de citas que tiene cada doctor*/
 SELECT doc.nombre_doctor, count(cm.codigo_citamedica)
 FROM public.doctor AS doc INNER JOIN public.cita_medica AS cm
 ON doc.cedula_doctor = cm.cedula_doctor GROUP BY doc.nombre_doctor;
 
-/* 6. Consulta las citas entre '2022-06-01' y '2022-08-31' */
+/* 7. Consulta las citas entre '2022-06-01' y '2022-08-31' */
 SELECT * FROM public.cita_medica AS cm
 WHERE cm.fecha_programada BETWEEN '2022-06-01' AND '2022-08-31';
 
-/* 7. Cuenta el número de medicamentos en cada formula*/
+/* 8. Cuenta el número de medicamentos en cada formula*/
 SELECT md.nombre_medicamento, COUNT(fm.codigo_medicamento)
 FROM public.medicamento AS md INNER JOIN public.formula_medica AS fm
 ON md.codigo_medicamento = fm.codigo_medicamento GROUP BY md.nombre_medicamento;
 
-/* 8. Consulta los doctores que sean especialistas en Dermatologia*/
+/* 9. Consulta los doctores que sean especialistas en Dermatologia*/
 SELECT doc.cedula_doctor, doc.nombre_doctor, doc.telefono_doctor, doc.correo_doctor
 FROM public.doctor AS doc INNER JOIN public.lista_especializaciones AS le
 ON doc.cedula_doctor = le.cedula_doctor INNER JOIN especializacion AS esp
 ON le.id_especializacion = esp.id_especializacion WHERE esp.nombre = 'Dermatología';
 
-/* 9. Consulta las citas entre '12:00' y '14:00' */
+/* 10. Consulta las citas entre '12:00' y '14:00' */
 SELECT * FROM public.cita_medica AS cm
 WHERE cm.hora_programada BETWEEN '12:00' AND '14:00';
 
-/* 10. Consulta las citas que tienen un resgistro de cita*/
+/* 11. Consulta las citas que tienen un resgistro de cita*/
 SELECT * FROM public.cita_medica as cm, public.registro_cita as rc
 WHERE cm.codigo_citamedica = rc.codigo_citamedica;
 
-/* 11. Consulta de los doctores que tengan más de una especializacion */
+/* 12. Consulta de los doctores que tengan más de una especializacion */
 SELECT cedula_doctor, nombre_doctor, telefono_doctor, correo_doctor FROM
 (SELECT doc.cedula_doctor, doc.nombre_doctor, doc.telefono_doctor, doc.correo_doctor,
 COUNT(le.id_especializacion) AS conteo_especializaciones FROM public.doctor AS doc
@@ -61,20 +61,39 @@ INNER JOIN public.lista_especializaciones AS le ON doc.cedula_doctor = le.cedula
 INNER JOIN especializacion AS esp ON le.id_especializacion = esp.id_especializacion
 GROUP BY doc.cedula_doctor) AS foo WHERE conteo_especializaciones > 1;
 
-/* 12. Consulta del total de cantidades solicitadas de un medicamento*/
+/* 13. Consulta del total de cantidades solicitadas de un medicamento*/
 SELECT md.nombre_medicamento AS Medicamento, SUM(fm.cantidad_solicitada) AS Suma_cantidades
 FROM public.medicamento AS md INNER JOIN public.formula_medica AS fm
 ON fm.codigo_medicamento = md.codigo_medicamento GROUP BY md.nombre_medicamento
 ORDER BY md.nombre_medicamento;
 
-/* 13. Consulta de las citas realizadas con el detalle */
+/* 14. Consulta de las citas realizadas con el detalle */
 SELECT pa.nombre_paciente, rc.detalle FROM public.paciente AS pa INNER JOIN public.cita_medica AS cm
 ON cm.cedula_paciente = pa.cedula_paciente INNER JOIN
 public.registro_cita AS rc ON rc.codigo_citamedica = cm.codigo_citamedica;
 
-/* 14. Consulta de las citas realizadas con el detalle y formula medica */
+/* 15. Consulta de las citas realizadas con el detalle y formula medica */
 SELECT pa.nombre_paciente, rc.detalle, md.nombre_medicamento, fm.indicaciones FROM public.paciente AS pa
 INNER JOIN public.cita_medica AS cm ON cm.cedula_paciente = pa.cedula_paciente
 INNER JOIN public.registro_cita AS rc ON rc.codigo_citamedica = cm.codigo_citamedica
 INNER JOIN public.formula_medica AS fm ON fm.codigo_registro = rc.codigo_registro
 INNER JOIN public.medicamento AS md ON md.codigo_medicamento = fm.codigo_medicamento;
+
+/* 16. Consulta de la maxima edad de un paciente*/
+SELECT MAX(pa.edad_paciente) as maxima_edad
+FROM public.paciente pa;
+
+/* 17. Consulta la minima edad que hay en los paciente */
+SELECT MIN(pa.edad_paciente) as minima_edad
+FROM public.paciente pa;
+
+/* 18. Promedio de edades en la tabla pacientes */
+SELECT AVG(pa.edad_paciente) as promedio_de_edades
+FROM public.paciente pa;
+
+/* 19. Seleccion de todos los datos en medicamentos */
+SELECT * FROM public.medicamento;
+
+/* 20. Promedio de la cantidad de medicamentos solicitados en las formulas */
+SELECT AVG(fm.cantidad_solicitada) AS promedio_de_medicamentos_en_formulas_medicas
+FROM public.formula_medica AS fm;
